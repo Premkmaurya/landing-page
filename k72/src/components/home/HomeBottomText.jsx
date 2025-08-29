1b0840bd7cef0bfc0db41ce33bc7722a04c5aef4
Premkmaurya <premkmaurya123456@gmail.com>
2025-08-30 00:56:46 +0530

    animation

--- k72/src/components/home/HomeBottomText.jsx
+++ k72/src/components/home/HomeBottomText.jsx
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom'
 function HomeBottomText() {
   return (
     <div className='font-[font-v-2] py-2 uppercase text-[4.8vw] flex items-center gap-5 text-center'>
-      <Link className="border-2 border-white rounded-full px-4 font-extralight hover:text-[#D3FD50] hover:border-[#D3FD50]" to="/projects">Projects</Link>
-      <Link className="border-2 border-white rounded-full px-4 font-extralight hover:text-[#D3FD50] hover:border-[#D3FD50]" to="/agence">Agence</Link>
+      <Link className="border-2 transition-all border-white rounded-full px-4 font-extralight hover:text-[#D3FD50] hover:border-[#D3FD50]" to="/projects">Projects</Link>
+      <Link className="border-2 transition-all border-white rounded-full px-4 font-extralight hover:text-[#D3FD50] hover:border-[#D3FD50]" to="/agence">Agence</Link>
     </div>
   )
 }
